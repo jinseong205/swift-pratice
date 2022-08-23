af97593ebd193fc07c7032455956c0f9e8a7e265
정진성 <jinseong205@gmail.com>
2022-08-23 19:01:53 +0000

    ^^7

--- pratice/day1.swift
+++ pratice/day1.swift
@@ -1,1 +1,10 @@
-print("hello world")
+var isDarkMode : Bool = false
+
+if(isDarkMode){
+	print("dark mode o")
+}else{
+	print("dark mode x")
+}
+
+var title : String = isDarkMode ? "darkMode o " : "darkMode x"
+print("title : \(title)")
